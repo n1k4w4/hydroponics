9b315f16b733a713b7428ce6236719976b7dc5ea
n1k4w4 <da-i_nikanico0213@outlook.jp>
2018-07-03 21:17:40 +0900

    change image.py

--- image.py
+++ image.py
@@ -4,31 +4,36 @@ from datetime import datetime
 
 if __name__ == '__main__':
     cam = cv2.VideoCapture(0)
+    b = True
 
-    last_time = datetime.now()
-    interval = 10
+    #last_time = datetime.now()
+    #interval = 60
 
     while True:
         ret,frame = cam.read()
 
         now = datetime.now()
-        fntime = lambda n : int(n.strftime('%S'))
-        sec = fntime(now) - fntime(last_time)
-
-        if sec < 0:
-            sec = sec * -1
-        #print(sec)
-
-        if sec > interval:
+        fntime = lambda n : int(n.strftime('%H'))
+       # sec = fntime(now) - fntime(last_time)
+        
+        
+        #if sec < 0:
+           # sec = sec * -1
+       # print(now, last_time)
+        if (fntime(now) % 4 == 0):
             f = now.strftime('%Y-%m-%d-%H-%M-%S') + ".jpg"
-            cv2.imwrite(f,frame)
-            last_time = now
-            print("save=",f)
+            if b:
+                cv2.imwrite(f,frame)
+                b = False
+                print("save={}, bool={}".format(f, b))
+           # last_time = now
+        else:
+            b = True
         
         cv2.imshow("Input",frame)
 
         key = cv2.waitKey(10)
 
-        if key == 27:
+        if key == 60:
             cv2.destroyAllWindows()
             break
